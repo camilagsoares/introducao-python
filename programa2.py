ae38467136bb37c0ed3c43d99462731e7150a2e5
camilagsoares <camila.soares19@hotmail.com>
2022-03-24 00:24:17 -0300

    Programas aula unidade 3

---
+++ programa2.py
@@ -0,0 +1,8 @@
+texto = "Alterando o valor de sep"
+
+print(texto)
+print('Coluna 1', 'Coluna 2', 'Coluna 3', sep = ' -- ')
+
+
+print(texto)
+print('Coluna 1', 'Coluna 2', 'Coluna 3', sep = ' -- ', end='...\n')
